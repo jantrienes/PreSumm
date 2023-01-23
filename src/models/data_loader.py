31199d70427bab38c562707843cde0c40de396a9
Jan Trienes <jan.trienes@uni-due.de>
2023-01-23 08:28:16 +0000

    Remove logging of dataloader

--- src/models/data_loader.py
+++ src/models/data_loader.py
@@ -79,8 +79,8 @@ def load_dataset(args, corpus_type, shuffle):
 
     def _lazy_dataset_loader(pt_file, corpus_type):
         dataset = torch.load(pt_file)
-        logger.info('Loading %s dataset from %s, number of examples: %d' %
-                    (corpus_type, pt_file, len(dataset)))
+        # logger.info('Loading %s dataset from %s, number of examples: %d' %
+        #             (corpus_type, pt_file, len(dataset)))
         return dataset
 
     # Sort the glob output by file name (by increasing indexes).
